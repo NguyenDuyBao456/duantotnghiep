58cae3315950aa0c622d80e82b735601f8a784a3
Nguyen Van Tien <tiennv01032004@gmail.com>
2025-01-18 00:27:12 +0700

    setup project

--- server/app/Models/Products.php
+++ server/app/Models/Products.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
     //
+
+    use HasFactory;
+
+    protected $fillable = ["name", "img", "categories"];
 }
